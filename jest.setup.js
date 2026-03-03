e10c63a687c67d01308eceeac327c7e1e4fe3372
dkaygithub <github@danielkay.org>
2026-03-03 22:09:20 +0000

    chore: robustly polyfill Request/Response and install node-fetch for CI

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,14 @@
 import '@testing-library/jest-dom'
-import 'whatwg-fetch'
+
+// Note: whatwg-fetch is primarily for browsers. For Node 18+, fetch is native,
+// but Jest environments sometimes need explicit global assignments.
+if (typeof global.Request === 'undefined') {
+    const nodeFetch = require('node-fetch') // Or just use native if available
+    global.Request = global.Request || nodeFetch.Request
+    global.Response = global.Response || nodeFetch.Response
+    global.Headers = global.Headers || nodeFetch.Headers
+    global.fetch = global.fetch || nodeFetch
+}
 
 const crypto = require('crypto')
 if (typeof global.crypto === 'undefined' || !global.crypto.subtle) {
@@ -9,19 +18,18 @@ if (typeof global.crypto === 'undefined' || !global.crypto.subtle) {
     });
 }
 
-import { TextEncoder, TextDecoder } from 'util'
+const { TextEncoder, TextDecoder } = require('util')
 global.TextEncoder = TextEncoder
 global.TextDecoder = TextDecoder
 
 if (typeof Response !== 'undefined' && !Response.json) {
     Response.json = function (body, init) {
-        const response = new Response(JSON.stringify(body), {
+        return new Response(JSON.stringify(body), {
             ...init,
             headers: {
                 'content-type': 'application/json',
                 ...(init ? init.headers : {})
             }
         });
-        return response;
     }
 }
